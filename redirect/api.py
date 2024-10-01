200d9df6c0152baeea3f4ca0e42186ef5f708647
Daniel Prange <daniel.prange@anders.com>
2024-10-01 13:49:57 +0300

    feat: add support for wildcard subpaths

--- redirect/api.py
+++ redirect/api.py
@@ -1,3 +1,6 @@
+from urllib.parse import urljoin
+
+from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect as django_redirect
 from ninja import Router
@@ -7,16 +10,40 @@ from redirect.models import Domain, RedirectRule
 router = Router()
 
 
-@router.get("/{path:value}")
-def redirect(request, value: str):
+@router.get("/{path:path}")
+def redirect(request, path: str):
     domain = get_object_or_404(Domain, name=request.get_host())
-    obj = get_object_or_404(RedirectRule, path__iexact=value.strip("/"), domain=domain)
+    try:
+        redirect_rule = get_object_or_404(
+            RedirectRule, path__iexact=path.strip("/"), domain=domain
+        )
+    except Http404:
+        wildcard_redirects = RedirectRule.objects.filter(
+            domain=domain, match_subpaths=True
+        )
+        for wildcard_rule in wildcard_redirects:
+            if path.startswith(wildcard_rule.path):
+                redirect_rule = wildcard_rule
+                break
+        else:
+            # No wildcard rule matched, raise 404
+            raise
 
-    destination = obj.destination
-    if obj.pass_query_string and (query_string := request.META.get("QUERY_STRING")):
+    destination = redirect_rule.destination
+
+    # Append subpath if needed
+    if redirect_rule.match_subpaths and redirect_rule.append_subpath:
+        # Needs to behave like /foo/(.*) -> someurl.com/(.*), where (.*) is the subpath
+        subpath = path.lstrip(redirect_rule.path)
+        destination = urljoin(destination, subpath)
+
+    # Append query string if needed
+    if redirect_rule.pass_query_string and (
+        query_string := request.META.get("QUERY_STRING")
+    ):
         destination += f"?{query_string}"
 
-    return django_redirect(destination, permanent=obj.permanent)
+    return django_redirect(destination, permanent=redirect_rule.permanent)
 
 
 @router.get("/")
--- redirect/tests/test_models.py
+++ redirect/tests/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.exceptions import ValidationError
 
 from redirect.models import RedirectRule
 
@@ -16,12 +17,19 @@ class TestRedirectRule:
 
         assert rule.path == "foo"
 
-    def test_save_calls_clean_method(self, domain):
-        rule = RedirectRule.objects.create(
+    def test_save_calls_clean_method(self, monkeypatch, domain):
+        call_count = 0
+
+        def mock_clean(*_, **__):
+            nonlocal call_count
+            call_count += 1
+
+        monkeypatch.setattr(RedirectRule, "clean", mock_clean)
+        RedirectRule.objects.create(
             domain=domain, path="/foo/", destination=self.DEFAULT_DESTINATION
         )
 
-        assert rule.path == "foo"
+        assert call_count == 1
 
     def test_clean_handles_empty_path(self, domain):
         rule = RedirectRule(
@@ -51,8 +59,67 @@ class TestRedirectRule:
             domain=other_domain, path="/foo", destination=self.DEFAULT_DESTINATION
         )
 
-        # This shouldn't
+        # This shouldn't be fine
         with pytest.raises(Exception):
             RedirectRule.objects.create(
                 domain=domain, path="/foo", destination=self.DEFAULT_DESTINATION
             )
+
+    @pytest.mark.parametrize(
+        "existing_path, conflicting_path",
+        [("/foo", "/foo/bar"), ("/foo/bar", "/foo")],
+    )
+    def test_match_subpaths_conflict_on_create(
+        self, domain, existing_path, conflicting_path
+    ):
+        RedirectRule.objects.create(
+            domain=domain,
+            path=existing_path,
+            destination=self.DEFAULT_DESTINATION,
+            match_subpaths=True,
+        )
+
+        with pytest.raises(ValidationError):
+            RedirectRule.objects.create(
+                domain=domain,
+                path=conflicting_path,
+                destination=self.DEFAULT_DESTINATION,
+                match_subpaths=True,
+            )
+
+    @pytest.mark.parametrize(
+        "existing_path, conflicting_path",
+        [("/foo", "/foo/bar"), ("/foo/bar", "/foo")],
+    )
+    def test_match_subpaths_conflict_on_update(
+        self, domain, existing_path, conflicting_path
+    ):
+        RedirectRule.objects.create(
+            domain=domain,
+            path=existing_path,
+            destination=self.DEFAULT_DESTINATION,
+            match_subpaths=True,
+        )
+        rule_to_update = RedirectRule.objects.create(
+            domain=domain,
+            path="/this-will-be-replaced",
+            destination=self.DEFAULT_DESTINATION,
+            match_subpaths=True,
+        )
+
+        rule_to_update.path = conflicting_path
+
+        with pytest.raises(ValidationError):
+            rule_to_update.save()
+
+    def test_match_subpaths_should_not_conflict_itself(self, domain):
+        # Amusingly enough, this could happen if updating a rule.
+        rule = RedirectRule.objects.create(
+            domain=domain,
+            path="/foo",
+            destination=self.DEFAULT_DESTINATION,
+            match_subpaths=True,
+        )
+
+        rule.path = "/foo/bar"
+        rule.save()
