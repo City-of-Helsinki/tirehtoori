b3384eed24f3e4a2974b3a9fd42ab2e6fc8c1892
Juha Louhiranta <juha.louhiranta@anders.fi>
2024-11-18 15:10:17 +0200

    chore(docker-entrypoint): wait for DB to be online
    Especially on local dev DB usually starts up slower than
    Django, which might try to run migrations as the first thing.
    
    Refs: RATYK-44

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# Wait for the database to be available
+if [ -z "$SKIP_DATABASE_CHECK" ] || [ "$SKIP_DATABASE_CHECK" = "0" ]; then
+  until nc -z -v -w30 "$DATABASE_HOST" "${DATABASE_PORT:-5432}"
+  do
+    echo "Waiting for postgres database connection..."
+    sleep 1
+  done
+  echo "Database is up!"
+fi
+
 if [[ "$APPLY_MIGRATIONS" = "True" ]]; then
     echo "Applying migrations..."
     ./manage.py migrate --noinput
